f14bd985341ff02f3155b7760147922160236157
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-28 23:25:43 -0300

    initial commits

---
+++ FriendList/FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Isaque da Silva on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
